9785703dc0cb08b8fd7c667f3575725e7f4e4945
afnanfauzi <afnanfauzihidayat@gmail.com>
2021-09-22 12:36:26 +0700

    Pembuatan table database, controller, dan model

---
+++ database/migrations/2021_09_22_042926_create_pemasukan_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePemasukanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemasukan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal');
+            $table->string('motif');
+            $table->string('barang');
+            $table->string('ukuran');
+            $table->string('keterangan');
+            $table->string('nama');
+            $table->string('jumlah');
+            $table->string('nominal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemasukan');
+    }
+}
---
+++ database/migrations/2021_09_22_043733_create_pengeluaran_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengeluaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengeluaran', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal');
+            $table->string('ab');
+            $table->string('instansi');
+            $table->string('konsumsi');
+            $table->string('transportasi');
+            $table->string('ket');
+            $table->string('nominal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengeluaran');
+    }
+}
